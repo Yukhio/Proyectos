872e293cc311a9686c48a9df058ec4edbfce6807
Yukhio <yukhio.000@gmail.com>
2025-06-11 18:02:24 -0600

    add(): Se agregó el modulo 2 y se modificó el primero

---
+++ Modulo_2.py
@@ -0,0 +1,29 @@
+# En este modulo comienza por ver los errores que pueden suceder con la sintaxis
+#Try
+try:
+    # Código que puede generar una excepción
+    resultado = 10 / 0  # División por cero
+    print(resultado)
+except ZeroDivisionError:
+    print("Error: División por cero")
+
+
+#Except
+try:
+    # Código que puede generar una excepción
+    resultado = 10 / 0  # División por cero
+    print(resultado)
+except ZeroDivisionError:
+    print("Error: División por cero")
+except ValueError:
+    print("Error: Valor inválido")
+
+#Finally
+try:
+    # Código que puede generar una excepción
+    archivo = open("archivo.txt", "r")
+    # Realizar operaciones con el archivo
+except FileNotFoundError:
+    print("Error: Archivo no encontrado")
+finally:
+    archivo.close()  # Cerrar el archivo siempre, incluso si ocurre una excepción
