ce407ad2df5743ca8647e5685998b9a4d86b2c74
Yukhio <yukhio.000@gmail.com>
2025-06-17 11:45:04 -0600

    Cambios

--- Modulo_2.py
+++ Modulo_2.py
@@ -18,12 +18,51 @@ except ZeroDivisionError:
 except ValueError:
     print("Error: Valor inválido")
 
+
 #Finally
-try:
-    # Código que puede generar una excepción
-    archivo = open("archivo.txt", "r")
-    # Realizar operaciones con el archivo
-except FileNotFoundError:
-    print("Error: Archivo no encontrado")
-finally:
-    archivo.close()  # Cerrar el archivo siempre, incluso si ocurre una excepción
+# try:
+#     # Código que puede generar una excepción
+#     archivo = open("archivo.txt", "r")
+#     # Realizar operaciones con el archivo
+# except FileNotFoundError:
+#     print("Error: Archivo no encontrado")
+# finally:
+#     archivo.close()  # Cerrar el archivo siempre, incluso si ocurre una excepción
+
+
+# # Excepciones persnalizadas
+# def funcion():
+#     # Código que puede generar una excepción personalizada
+#     if condicion:
+#         raise Exception("Descripción del error")
+# try:
+#     funcion()
+# except Exception as e:
+#     print(f"Error: {str(e)}")
+
+
+# Entradas/Salidas
+# En las entradas supongamos que es como un formulario donde el usuario llena datos
+nombre = input("Ingresa tu nombre: ")
+edad = int(input("Ingresa tu edad: "))
+
+if edad >= 18:
+    print(nombre+" eres mayor de edad.")
+else:
+    print(nombre+" eres menor de edad.")
+
+
+print("Hola, " + nombre + "!")
+print("Tienes " + str(edad) + " años.")
+
+
+# Escritura de archivos
+archivo = open("archivo_creado_python.txt", "w")
+archivo.write("Nuevo archivo creado llamado" + archivo.name)
+archivo.close()
+
+# Lectura de archivos
+archivo = open("datos.txt", "r")
+contenido = archivo.read()
+print(contenido)
+archivo.close()
